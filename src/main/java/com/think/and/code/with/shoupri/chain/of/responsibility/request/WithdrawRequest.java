b11bbdb88692f18425a000df3353c4670a0ba50d
Shouvik <shouvikdutta1999@gmail.com>
2025-12-27 11:20:30 +0530

    [CHORE] Fixed client de-srialize response exception

--- src/main/java/com/think/and/code/with/shoupri/chain/of/responsibility/request/WithdrawRequest.java
+++ src/main/java/com/think/and/code/with/shoupri/chain/of/responsibility/request/WithdrawRequest.java
@@ -1,8 +1,12 @@
 package com.think.and.code.with.shoupri.chain.of.responsibility.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class WithdrawRequest {
     private Long amount;
 }
--- src/main/java/com/think/and/code/with/shoupri/chain/of/responsibility/response/MoneyDenomination.java
+++ src/main/java/com/think/and/code/with/shoupri/chain/of/responsibility/response/MoneyDenomination.java
@@ -1,10 +1,14 @@
 package com.think.and.code.with.shoupri.chain.of.responsibility.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MoneyDenomination {
     private Long thousandNoteCount;
     private Long fiveHundredNoteCount;
